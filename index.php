e514a1f2c49f445265e46f47750d3693a3ce8ed0
Anatoliy Shestopalov <shestopalov92@gmail.com>
2023-02-06 12:59:37 +0300

    add, feat: index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>(testing)Index</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css">
+</head>
+<body>
+
+    <h1>Home page</h1>
+    <a href="signup-forms.html">Signup</a>
+</body>
+</html>
